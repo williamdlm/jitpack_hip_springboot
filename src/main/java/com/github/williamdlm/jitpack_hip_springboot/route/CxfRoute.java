9a70423610a5d3580ba207767c942f62eba24e71
William Mota <william.mota.ext@spms.min-saude.pt>
2024-06-10 11:11:50 +0100

    Put BindToRegistry into class that extends RouteBuilder

--- src/main/java/com/github/williamdlm/jitpack_hip_springboot/route/CxfRoute.java
+++ src/main/java/com/github/williamdlm/jitpack_hip_springboot/route/CxfRoute.java
@@ -1,14 +1,25 @@
 package com.github.williamdlm.jitpack_hip_springboot.route;
 
+import com.github.williamdlm.jitpack_hip_springboot.service.GreetingService;
+import org.apache.camel.BindToRegistry;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.cxf.jaxws.CxfEndpoint;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CxfRoute extends RouteBuilder {
 
+    @BindToRegistry("testando")
+    public CxfEndpoint greetingEndpoint() {
+        CxfEndpoint cxfEndpoint = new CxfEndpoint();
+        cxfEndpoint.setAddress("/greeting");
+        cxfEndpoint.setServiceClass(GreetingService.class);
+        return cxfEndpoint;
+    }
+
     @Override
     public void configure() throws Exception {
-        from("cxf:bean:greetingEndpoint")
+        from("cxf:bean:testando")
                 .log("{$body}")
                 .to("bean:greetingServiceImpl?method=sayHi");
     }
